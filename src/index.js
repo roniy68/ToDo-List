4218fe6de4b83a07b2a477180d9c57575f9cff59
Anna Petras <apetras2017@gmail.com>
2022-12-16 18:54:00 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import './style.css';
 
 const toDoTasks = [
-    {
-      description: 'Create ToDo list',
-      completed: false,
-      index: 0,
-    },
-    {
-      description: 'Revise HTML and CSS course',
-      completed: true,
-      index: 1,
-    },
-    {
-      description: 'Finish webpack project',
-      completed: false,
-      index: 2,
-    },
-    {
-      description: 'Create professional CV',
-      completed: false,
-      index: 3,
-    },
-  ];
+  {
+    description: 'Create ToDo list',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Revise HTML and CSS course',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Finish webpack project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Create professional CV',
+    completed: false,
+    index: 3,
+  },
+];
 
 const listContent = document.querySelector('.list-content');
 
 // populate list items
 const populateListItems = (arrayOfTasks) => {
-    listContent.innerHTML = '';
-    arrayOfTasks.forEach((task) => {
-      listContent.innerHTML += `
+  listContent.innerHTML = '';
+  arrayOfTasks.forEach((task) => {
+    listContent.innerHTML += `
       <li>
         <button><i class="fa-solid fa-check ${task.completed ? 'active' : ''}"></i></button>
         <div class="container">
@@ -38,9 +38,9 @@ const populateListItems = (arrayOfTasks) => {
         <i class="fa-solid fa-ellipsis-vertical"></i>
       </li>
       `;
-    });
-  };
+  });
+};
   // Sort array by indexes
-  const sortArray = (arr) => arr.sort((a, b) => a.index - b.index);
-  
-  populateListItems(sortArray(toDoTasks));
+const sortArray = (arr) => arr.sort((a, b) => a.index - b.index);
+
+populateListItems(sortArray(toDoTasks));
