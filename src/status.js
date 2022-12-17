faacd51d85478a8a858fd85c4b4af0fec4e95db6
Anna Petras <apetras2017@gmail.com>
2022-12-17 14:57:20 +0100

    Refactor index.html

--- src/status.js
+++ src/status.js
@@ -1,20 +1,20 @@
 export default (toDoTasks) => {
-  const checkBtns = document.querySelectorAll('.check-btn');
+  const checkBtns = document.querySelectorAll(".check-btn");
   checkBtns.forEach((checkBtn, index) => {
-    checkBtn.addEventListener('click', () => {
-      checkBtn.childNodes[0].classList.toggle('active');
-      checkBtn.nextElementSibling.classList.toggle('active');
-      if (checkBtn.childNodes[0].classList.contains('active')) {
+    checkBtn.addEventListener("click", () => {
+      checkBtn.childNodes[0].classList.toggle("active");
+      checkBtn.nextElementSibling.classList.toggle("active");
+      if (checkBtn.childNodes[0].classList.contains("active")) {
         toDoTasks.modifyTask(
           index,
           checkBtn.nextElementSibling.textContent.trim(),
-          true,
+          true
         );
       } else {
         toDoTasks.modifyTask(
           index,
           checkBtn.nextElementSibling.textContent.trim(),
-          false,
+          false
         );
       }
     });
